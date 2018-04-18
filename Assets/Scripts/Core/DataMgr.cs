31e2a2b2de81cdc5fe2df3e6a6377803a6a5d055
Recho <springworder@gmail.com>
2018-04-18 17:09:02 +0800

    增加player退出方法，并增加一些和异步相对的同步方法

--- Assets/Scripts/Core/DataMgr.cs
+++ Assets/Scripts/Core/DataMgr.cs
@@ -54,11 +54,11 @@ public class DataMgr
 
 
     /// <summary>
-    /// 判断是否能注册
+    /// 判断是否能注册，异步调用
     /// </summary>
     /// <param name="id"></param>
     /// <returns></returns>
-    public async Task<bool> CanRegister(string id)
+    public async Task<bool> CanRegisterAsync(string id)
     {
         //判断防止sql注入
         if (!IsSafeStr(id))
@@ -83,19 +83,48 @@ public class DataMgr
     }
 
     /// <summary>
+    /// 判断能否注册
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public bool CanRegister(string id)
+    {
+        //判断防止sql注入
+        if (!IsSafeStr(id))
+            return false;
+
+        string commandStr = $"select * from user where id='{id}';";
+        MySqlCommand sqlCommand = new MySqlCommand(commandStr, sqlConn);
+
+        try
+        {
+            var reader =sqlCommand.ExecuteReader();
+
+            bool hasRows = reader.HasRows;
+            reader.Close();
+            return !hasRows;
+        }
+        catch (System.Exception)
+        {
+            return false;
+        }
+
+    }
+
+    /// <summary>
     /// 异步注册方法
     /// </summary>
     /// <param name="id"></param>
     /// <param name="pw"></param>
     /// <returns></returns>
-    public async Task<bool> Register(string id,string pw)
+    public async Task<bool> RegisterAsync(string id,string pw)
     {
         if (!IsSafeStr(id) || !IsSafeStr(pw))
         {
             Debug.LogError("输入了非法字符");
             return false;
         }
-        var resultReg = await CanRegister(id);
+        var resultReg = await CanRegisterAsync(id);
 
         if (resultReg == false)
         {
@@ -121,11 +150,49 @@ public class DataMgr
     }
 
     /// <summary>
-    /// 创建角色
+    /// 注册用户信息
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="pw"></param>
+    /// <returns></returns>
+    public bool Register(string id, string pw)
+    {
+        if (!IsSafeStr(id) || !IsSafeStr(pw))
+        {
+            Debug.LogError("输入了非法字符");
+            return false;
+        }
+        var resultReg =CanRegister(id);
+
+        if (resultReg == false)
+        {
+            Debug.LogError("id已被注册");
+            return false;
+        }
+
+        string cmdStr = $"insert into user set id='{id}',pw='{pw}'";
+
+        MySqlCommand sqlCommand = new MySqlCommand(cmdStr, sqlConn);
+
+        try
+        {
+            int result =sqlCommand.ExecuteNonQuery();
+
+            return result == 1;
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 创建角色，异步方法
     /// </summary>
     /// <param name="id"></param>
     /// <returns></returns>
-    public async Task<bool> CreatePlayer(string id)
+    public async Task<bool> CreatePlayerAsync(string id)
     {
         //防止sql注入
         if (!IsSafeStr(id))
@@ -169,12 +236,60 @@ public class DataMgr
     }
 
     /// <summary>
-    /// 根据id和pw检查登陆信息
+    /// 创建角色
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public bool CreatePlayer(string id)
+    {
+        //防止sql注入
+        if (!IsSafeStr(id))
+            return false;
+
+        //序列化
+        PlayerData playerData = new PlayerData();
+
+        IFormatter formatter = new BinaryFormatter();
+        MemoryStream stream = new MemoryStream();
+        try
+        {
+            formatter.Serialize(stream, playerData);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+            return false;
+        }
+        //从流中获取byte
+        byte[] byteArr = stream.ToArray();
+        stream.Close();
+
+        //写入数据库
+        string cmdStr = $"insert into player set id='{id}',data=@data;";
+        MySqlCommand sqlCommand = new MySqlCommand(cmdStr, sqlConn);
+        //使用参数方式 byte不好使用sql语句
+        sqlCommand.Parameters.Add("@data", MySqlDbType.Blob);
+        sqlCommand.Parameters["@data"].Value = byteArr;
+
+        try
+        {
+            var result =sqlCommand.ExecuteNonQuery();
+            return result == 1;
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"创建角色失败,{e.Message}");
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 根据id和pw检查登陆信息,异步方法
     /// </summary>
     /// <param name="id"></param>
     /// <param name="pw"></param>
     /// <returns></returns>
-    public async Task<bool> CheckPassword(string id ,string pw)
+    public async Task<bool> CheckPasswordAsync(string id ,string pw)
     {
         //检查sql注入
         if (!IsSafeStr(id) || !IsSafeStr(pw))
@@ -187,6 +302,37 @@ public class DataMgr
         try
         {
             var reader =await sqlCommand.ExecuteReaderAsync();
+            bool hasData = reader.HasRows;
+            reader.Close();
+
+            return hasData;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 检查登陆信息
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="pw"></param>
+    /// <returns></returns>
+    public bool CheckPassword(string id, string pw)
+    {
+        //检查sql注入
+        if (!IsSafeStr(id) || !IsSafeStr(pw))
+            return false;
+
+        //查询数据库
+        string cmdStr = $"select * from user where id='{id}' and pw='{pw}';";
+        MySqlCommand sqlCommand = new MySqlCommand(cmdStr, sqlConn);
+
+        try
+        {
+            var reader =sqlCommand.ExecuteReader();
             bool hasData = reader.HasRows;
             reader.Close();
 
@@ -265,11 +411,11 @@ public class DataMgr
     }
 
     /// <summary>
-    /// 保存玩家数据
+    /// 保存玩家数据，异步方法
     /// </summary>
     /// <param name="player"></param>
     /// <returns></returns>
-    public async Task<bool> SavePlayer(Player player)
+    public async Task<bool> SavePlayerAsync(Player player)
     {
         string id = player.id;
         PlayerData playerData = player.data;
@@ -299,6 +445,50 @@ public class DataMgr
         try
         {
             var result = await sqlCommand.ExecuteNonQueryAsync();
+            return result == 1;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 同步保存方法
+    /// </summary>
+    /// <param name="player"></param>
+    /// <returns></returns>
+    public  bool SavePlayer(Player player)
+    {
+        string id = player.id;
+        PlayerData playerData = player.data;
+
+        //序列化
+        IFormatter formatter = new BinaryFormatter();
+        MemoryStream stream = new MemoryStream();
+        try
+        {
+            formatter.Serialize(stream, playerData);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+
+            return false;
+        }
+        //输出到byte数组
+        byte[] byteArr = stream.ToArray();
+
+        //更新数据库信息
+        string cmdStr = $"update player set data=@data where id='{id}'";
+        MySqlCommand sqlCommand = new MySqlCommand(cmdStr, sqlConn);
+        sqlCommand.Parameters.Add("@data", MySqlDbType.Blob);
+        sqlCommand.Parameters["@data"].Value = byteArr;
+
+        try
+        {
+            var result =sqlCommand.ExecuteNonQuery();
             return result == 1;
         }
         catch (Exception e)
--- Assets/Scripts/Logic/Player.cs
+++ Assets/Scripts/Logic/Player.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
+/// <summary>
+/// 玩家角色类，玩家下线，保存角色数据，向角色发送消息
+/// </summary>
 public class Player
 {
     //角折id
@@ -12,4 +16,91 @@ public class Player
     public PlayerData data;
     //玩家的临时数据
     public PlayerTempData tempData;
+
+    public Player(string _id, Conn _conn)
+    {
+        this.id = _id;
+        this.conn = _conn;
+        this.tempData = new PlayerTempData();
+    }
+
+    //发送
+    /// <summary>
+    /// 对ServNet中Send的封装
+    /// </summary>
+    /// <param name="protocol">使用的协议</param>
+    public void Send(ProtocolBase protocol)
+    {
+        if (conn == null)
+            return;
+        ServNet.instance.Send(conn, protocol);
+    }
+
+    /// <summary>
+    /// 将玩家踢下线
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="protocol"></param>
+    /// <returns></returns>
+    public static bool KickOff(string id,ProtocolBase protocol)
+    {
+        Conn[] conns = ServNet.instance.conns;
+
+        for (int i = 0; i < conns.Length; i++)
+        {
+            if (conns[i] == null || conns[i].isUse == false || conns[i].player == null)
+                continue;
+            if(conns[i].player.id.Equals(id))
+            {
+                lock (conns[i].player)
+                {
+                    if (protocol != null)
+                        conns[i].player.Send(protocol);
+                    return conns[i].player.Logout();
+                }
+            }
+
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// 下线并保存数据
+    /// </summary>
+    /// <returns></returns>
+    public  bool Logout()
+    {
+        //事件处理
+        //调用ServNet的HandlePlayerEvent
+        //TODO:还未实现
+
+        //保存数据
+        //异步方法调用
+        var result =DataMgr.instance.SavePlayer(this);
+        if (result == false)
+            return false;
+
+        //下线
+        conn.player = null;
+        conn.Close();
+        return true;
+    }
+
+    public async Task<bool> LogoutAsync()
+    {
+        //事件处理
+        //调用ServNet的HandlePlayerEvent
+        //TODO:还未实现
+
+        //保存数据
+        //异步方法调用
+        var result =await DataMgr.instance.SavePlayerAsync(this);
+        if (result == false)
+            return false;
+
+        //下线
+        conn.player = null;
+        conn.Close();
+        return true;
+    }
 }
