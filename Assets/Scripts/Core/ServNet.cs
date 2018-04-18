17e05ab1c11551106bc7ae34a1e1ec21b8f3d17b
Recho <springworder@gmail.com>
2018-04-18 10:58:39 +0800

    网路处理类

---
+++ Assets/Scripts/Core/ServNet.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+/// <summary>
+/// 网络处理类
+/// </summary>
+public class ServNet
+{
+    //监听套接字
+    public Socket listenfd;
+    //客户端连接池
+    public Conn[] conns;
+    //最大连接数
+    public int maxConn = 50;
+    //单例
+    public static ServNet instance;
+
+    //构造函数
+    public ServNet()
+    {
+        instance = this;
+    }
+
+    /// <summary>
+    /// 获取可用连接对象的索引
+    /// </summary>
+    /// <returns>返回连接对象在池中的索引，返回-1代表获取失败</returns>
+    public int NewIndex()
+    {
+        int index=-1;
+        if (conns == null)
+            return -2;
+        for (int i = 0; i < conns.Length; i++)
+        {
+            if (!conns[i].isUse)
+            {
+                index = i;
+                if (conns[i] == null)
+                    conns[i] = new Conn();
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    /// <summary>
+    /// 启动服务
+    /// </summary>
+    /// <param name="host">主机的ip</param>
+    /// <param name="port">绑定端口号</param>
+    public void Start(string host,int port)
+    {
+        //初始化conns
+        conns = new Conn[maxConn];
+
+        for (int i = 0; i < conns.Length; i++)
+        {
+            conns[i] = new Conn();
+        }
+        //初始化socket
+        listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //Bind
+        IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(host), port);
+        listenfd.Bind(iPEndPoint);
+        //监听
+        listenfd.Listen(maxConn);
+        //开启接受连接
+        listenfd.BeginAccept(AccetpCb, null);
+        Debug.Log($"服务器启动成功,绑定ip为:{host},绑定地址为{port}");
+    }
+
+    /// <summary>
+    /// 异步回调
+    /// </summary>
+    /// <param name="ar"></param>
+    public void AccetpCb(IAsyncResult ar)
+    {
+        try
+        {
+            Socket socket = listenfd.EndAccept(ar);
+            int index = NewIndex();
+            if(index<0)
+            {
+                socket.Close();
+                Debug.Log($"无可用的连接，连接池异常");
+            }
+            else
+            {
+                Conn conn = conns[index];
+                conn.Init(socket);
+                Debug.Log($"{socket.RemoteEndPoint.ToString()}---用户已连接---");
+                conn.socket.BeginReceive(conn.readBuff, conn.buffCount, conn.BuffRemain(), SocketFlags.None,ReceiveCb ,conn);
+                listenfd.BeginAccept(AccetpCb, null);
+
+            }
+        }
+        catch ( Exception e)
+        {
+            Debug.LogError($"异步接收错误：{e.Message}");
+        }
+    }
+
+    /// <summary>
+    /// 异步接收消息
+    /// </summary>
+    /// <param name="ar"></param>
+    private void ReceiveCb(IAsyncResult ar)
+    {
+        Conn conn = ar.AsyncState as Conn;
+        lock(conn)
+        {
+            try
+            {
+                int count = conn.socket.EndReceive(ar);
+                //关闭信号
+                if(count<0)
+                {
+                    //断开 连接
+                    Debug.Log($"{conn.socket.RemoteEndPoint.ToString()}---断开连接---");
+                    conn.Close();
+                    return;
+                }
+                conn.buffCount += count;
+                //交给处理类
+                //TODO:
+                //ProcessData(conn);
+                //继续接收
+                conn.socket.BeginReceive(conn.readBuff, conn.buffCount, conn.BuffRemain(),SocketFlags.None,ReceiveCb, conn);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"异步接收异常,{e.Message}");
+            }
+        }
+    }
+
+    /// <summary>
+    /// 关闭
+    /// </summary>
+    public void Close()
+    {
+        for (int i = 0; i < conns.Length; i++)
+        {
+            Conn conn = conns[i];
+            if (conn == null)
+                continue;
+            if (!conn.isUse)
+                continue;
+
+            //加锁 避免线程竞争 
+            //访问conn的线程有
+            //主线程
+            //异步回调
+            //心跳的定时器线程
+            lock (conn)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
--- Assets/Scripts/Logic/Conn.cs
+++ Assets/Scripts/Logic/Conn.cs
@@ -1,16 +1,114 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
-public class Conn : MonoBehaviour {
+/// <summary>
+/// 连接对象
+/// </summary>
+public class Conn
+{
+    //常量
+    public const int BUFFER_SIZE = 1024;
+    //socket
+    public Socket socket;
+    //是否使用
+    public bool isUse = false;
+    //Buff
+    public byte[] readBuff = new byte[BUFFER_SIZE];
+    //buff大小
+    public int buffCount = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //粘包分包
+    public byte[] lenBytes = new byte[sizeof(UInt32)];
+    public Int32 msgLength = 0;
+
+    //心跳时间
+    public long lastTickTime = long.MinValue;
+
+    //player对象
+    public Player player;
+
+    //无参数构造函数用来初始化工作
+    public Conn()
+    {
+        readBuff = new byte[BUFFER_SIZE];
+    }
+
+    /// <summary>
+    /// 初始化函数，接收传入的socket初始socket连接等需要的数据
+    /// </summary>
+    public void Init(Socket _socket)
+    {
+        this.socket = _socket;
+        isUse = true;
+        buffCount = 0;
+        //心跳时间
+        //lastTickTime = Sys.GetTimeStamp();
+    }
+
+    /// <summary>
+    /// 剩余的大小
+    /// </summary>
+    /// <returns></returns>
+    public int BuffRemain()
+    {
+        return BUFFER_SIZE - buffCount;
+    }
+    /// <summary>
+    /// 获取地址
+    /// </summary>
+    /// <returns>返回地址字符串</returns>
+    public string GetAddress()
+    {
+        if (!isUse)
+            return "无法获取地址";
+        return socket.RemoteEndPoint.ToString();
+    }
+    
+    /// <summary>
+    /// 关闭连接对象，重置一些参数
+    /// </summary>
+    public void Close()
+    {
+        if (!isUse)
+            return;
+        if(player!=null)
+        {
+            //玩家退出操作
+            //player.Logout();
+            return;
+        }
+        Debug.Log($"{GetAddress()}---断开连接---");
+        //断开socket
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+        isUse = false;
+    }
+
+    /// <summary>
+    /// 对缓冲区消息处理函数
+    /// </summary>
+    /// <param name="conn"></param>
+    public void ProcessData(Conn conn)
+    {
+        //如果消息小于一定长度 直接不处理
+        if (conn.buffCount < sizeof(Int32))
+            return;
+
+        //获取一个消息体的总长度
+        
+    }
+
+    //发送协议
+    //public void Send(ProtocolBase protocol)
+    //{
+            //Servnet.instance.Send(this,protocol);
+    //}
+
+    public void Test()
+    {
+        
+    }
 }
