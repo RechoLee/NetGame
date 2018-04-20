1a42efa40d688034a5e2a43d9282449768e3f379
Recho <springworder@gmail.com>
2018-04-20 18:59:48 +0800

    服务器对一个场景对象的定义

---
+++ Assets/Scripts/Server/Scene.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 场景类 一个场景中可以其中包含对场景中玩家的操作
+/// </summary>
+public class Scene
+{
+    public static Scene instance;
+
+    public Scene()
+    {
+        instance = this;
+    }
+
+    List<ScenePlayer> playerList = new List<ScenePlayer>();
+
+    /// <summary>
+    /// 根据id返回玩家信息
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public ScenePlayer GetPlayer(string id)
+    {
+        return null;
+    }
+
+    /// <summary>
+    /// 加入玩家
+    /// </summary>
+    /// <param name="id"></param>
+    public void AddPlayer(string id)
+    {
+
+    }
+
+    /// <summary>
+    /// 删除玩家
+    /// </summary>
+    /// <param name="id"></param>
+    public void DelPlayer(string id)
+    {
+
+    }
+
+    /// <summary>
+    /// 发送玩家列表
+    /// </summary>
+    /// <param name="player"></param>
+    public void SendPlayerList(Player player)
+    {
+
+    }
+
+    /// <summary>
+    /// 更新场景中玩家的信息
+    /// </summary>
+    /// <param name="di"></param>
+    /// <param name="pos"></param>
+    /// <param name="score"></param>
+    public void UpdateInfo(string di,Pos pos,int score)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Server/ScenePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 场景中玩家的定义
+/// </summary>
+public class ScenePlayer
+{
+    public string id;
+
+    public Pos pos = new Pos {
+        X = 0f,
+        Y=0f,
+        Z=0f
+    };
+
+    /// <summary>
+    /// 玩家分数
+    /// </summary>
+    public int score = 0;
+}
+
+public class Pos
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+}
