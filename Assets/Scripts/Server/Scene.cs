95e8c1d08fa1fa262b6089a5275c7c477e226341
Recho <springworder@gmail.com>
2018-04-24 17:06:38 +0800

    场景类完善

--- Assets/Scripts/Server/Scene.cs
+++ Assets/Scripts/Server/Scene.cs
@@ -17,12 +17,19 @@ public class Scene
     List<ScenePlayer> playerList = new List<ScenePlayer>();
 
     /// <summary>
-    /// 根据id返回玩家信息
+    /// 根据id返回玩家信息 没有找到返回null
     /// </summary>
     /// <param name="id"></param>
     /// <returns></returns>
     public ScenePlayer GetPlayer(string id)
     {
+        for(int i=0;i<playerList.Count;i++)
+        {
+            if(id.Equals(playerList[i].id))
+            {
+                return playerList[i];
+            }
+        }
         return null;
     }
 
@@ -30,27 +37,63 @@ public class Scene
     /// 加入玩家
     /// </summary>
     /// <param name="id"></param>
-    public void AddPlayer(string id)
+    public void AddPlayer(string _id)
     {
-
+        lock (playerList)
+        {
+            ScenePlayer player = new ScenePlayer
+            {
+                id = _id
+            };
+            playerList.Add(player);
+        }
     }
 
     /// <summary>
     /// 删除玩家
     /// </summary>
     /// <param name="id"></param>
-    public void DelPlayer(string id)
+    public void DelPlayer(string _id)
     {
+        lock (playerList)
+        {
+            ScenePlayer p = GetPlayer(_id);
+            if (p == null)
+                return;
 
+            playerList.Remove(p);
+
+            ///删除玩家成功后 通知服务器广播此玩家离开的消息
+            ProtocolBytes protocol = new ProtocolBytes();
+            protocol.AddString("PlayerLeave");
+            protocol.AddString(_id);
+            ServNet.instance.Broadcast(protocol);
+        }
     }
 
     /// <summary>
-    /// 发送玩家列表
+    /// 发送玩家列表 一个协议Get List
     /// </summary>
     /// <param name="player"></param>
     public void SendPlayerList(Player player)
     {
+        int count = playerList.Count;
+        //构造GetList协议
+        ProtocolBytes protocol = new ProtocolBytes();
+        protocol.AddString("GetList");
+        protocol.AddInt(count);
+        for (int i = 0; i < count; i++)
+        {
+            ScenePlayer p = playerList[i];
+            protocol.AddString(p.id);
+            protocol.AddFloat(p.pos.X);
+            protocol.AddFloat(p.pos.Y);
+            protocol.AddFloat(p.pos.Z);
+            protocol.AddInt(p.score);
+        }
 
+        //向玩家发送协议
+        player.Send(protocol);
     }
 
     /// <summary>
@@ -59,8 +102,17 @@ public class Scene
     /// <param name="di"></param>
     /// <param name="pos"></param>
     /// <param name="score"></param>
-    public void UpdateInfo(string di,Pos pos,int score)
+    public void UpdateInfo(string id, Pos pos,int score)
     {
+        int count = playerList.Count;
+        ProtocolBytes protocol = new ProtocolBytes();
+        ScenePlayer p = GetPlayer(id);
+        if (p == null)
+            return;
 
+        p.pos.X = pos.X;
+        p.pos.Y = pos.Y;
+        p.pos.Z = pos.Z;
+        p.score = score;
     }
 }
